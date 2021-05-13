744af3359d765f627366f287fcfe3a51e6cbdfd7
PabloGratas <39206230+PabloGratas@users.noreply.github.com>
2021-05-12 22:24:22 -0700

    IGNOR EME

---
+++ src/testing/testing1.js
@@ -0,0 +1,17 @@
+function greatFunction(x, y){
+let z =        x +    y;
+console.log(z)              ;
+let squares = x*x + y  * y;
+console.log(squares);
+}
+function worseFunction   ( ) 
+
+{
+console.log(testing);
+
+    }
+
+    function sumUpTo(x){
+        if(x == 0) return 0;
+        else 
+        return x + sum;}
