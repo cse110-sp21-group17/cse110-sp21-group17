18ca7c4bf8a480c2405130bd18bc01b9f0670030
JasonSatherr <jsather@ucsd.edu>
2021-05-28 20:52:39 -0700

    removed ~50 line mess of comments

--- src/nodeobject/create.test.js
+++ src/nodeobject/create.test.js
@@ -129,37 +129,5 @@ describe('Creation of tasks and goals', function(){
 
     });
 
-    // test('checks to make a second note is retrievable', () => {
-    //     //new Date(year, monthIndex, day, hours, minutes, seconds, milliseconds);
-    //     setTimeout(1000);
-    //     let date = Date(2077, 12, 10, 4, 4, 4, 4);
-    //     let noteJSON = creationTool.createNote("DAY 2 in hell: I have just signed up for another quarter of hurt.", date);
-    //     //let storedNote = window.localStorage.getItem(noteJSON.key);
-    //     let keyArrString = localStorage.getItem(date);
-    //     expect(keyArrString).not.toBe(null);
-    //     let keyArr = JSON.parse(keyArrString);
-    //     expect(keyArr[1]).not.toBe(null);
-    //     expect(keyArr[1]).toBe(noteJSON.key.toString());
-    //     expect(localStorage.getItem(keyArr[1])).not.toBe(null);
-    //     //window.localStorage.clear();
-    // });
-
-    // test('checks to make sure the first note holds the content and date properly', () => {
-    //     let date = Date(2077, 12, 10, 4, 4, 4, 4);
-    //     let keyArrString = localStorage.getItem(date);
-    //     expect(keyArrString).not.toBe(null);
-    //     let keyArr = JSON.parse(keyArrString);
-    //     let noteString = localStorage.getItem(keyArr[1]);
-
-    //     //fetched the note created in the last test
-    //     expect(noteString).not.toBe(null);
-    //     var storedNoteJSON = JSON.parse(noteString);
-    //     expect(storedNoteJSON.cnt).toMatch("DAY 2 in hell: I have just signed up for another quarter of hurt.");
-    //     expect(storedNoteJSON.cnt).not.toMatch("DAY 1 in hell: I have just signed up for another quarter of hurt.");
-    //     expect(storedNoteJSON.dte).toBe(date);
-    // });
-
-    //WOOOOOOOOOOOOOOOOOOOOOOOOO ~ if I wanna be more rigorous I'd try to add with different dates.
-    //Also, note to self, I need to tell my team that we need the date to be an object that contains year month and day data.
     
 });
