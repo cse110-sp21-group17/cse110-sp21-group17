9e41a6c2be50e0813b6c3f7849780a557a49a1b0
Jennie Ding <1589423201@qq.com>
2021-06-02 00:56:29 -0700

    remove the force navigation on main page

--- src/main.js
+++ src/main.js
@@ -24,6 +24,8 @@ window.addEventListener('popstate', (e) => {
 });
 
 window.addEventListener('load', function () {
+
+
     // if user already login in
-     router.setState("createPage");
+    // router.setState("createPage");
 })
