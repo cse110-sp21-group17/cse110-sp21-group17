e0db7fa7ae2d3fbc2ef4241887a7924a98d85b27
JasonSatherr <jsather@ucsd.edu>
2021-06-05 22:44:14 -0700

    Created new test files

---
+++ src/__tests__/userDB.test.js
@@ -0,0 +1,7 @@
+var creationTool = require('../userDB');
+
+describe('Creation of notes', function(){
+    // test('checks to make sure there is not a note named note tester', () => {
+    //     expect(window.localStorage.getItem('noteTester')).toBe(null);
+    // });
+});
