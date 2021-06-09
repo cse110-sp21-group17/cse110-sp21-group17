bdcbe340b052517fbe3ff9d4165ce1e2e23ae3b1
JasonSatherr <jsather@ucsd.edu>
2021-06-09 15:59:15 -0700

    Fixed tag so that jsdocs can parse it

--- src/userDB.js
+++ src/userDB.js
@@ -148,7 +148,7 @@ export function completeTask(toComplete){
 /**
  * Returns all entries on a date.
  * @param {Date} dateIndex - The date object you want to access 
- * @returns {[[Task], [Subtask], [Note], [Event], [Goal]]}
+ * @returns {Array containing arrays of Tasks, Subtasks, Notes, Events, Goals}
  */
 export async function getDateEntries(dateIndex) { //Get all of the entries on a given date
     let taskArr = await db.tasks.where("entryDate").equals(dateIndex).toArray();
