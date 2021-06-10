fbf7ed75ec80e0a00a09639e026756620301f139
JasonSatherr <jsather@ucsd.edu>
2021-06-10 11:58:11 -0700

    created power function

--- src/CI_Tester.js/sum_difference.js
+++ src/CI_Tester.js/sum_difference.js
@@ -1,21 +1,31 @@
 /**
- * 
+ *
  * @param {int} a First param to add
- * @param {int} b Second param to add 
+ * @param {int} b Second param to add
  * @returns {int} The sum
  */
- function sum(a, b) {
-    return a + b;
-  }
-  
+function sum(a, b) {
+  return a + b;
+}
+
 /**
- * 
+ *
  * @param {int} a First param to multiply
- * @param {int} b Second param to multiply 
+ * @param {int} b Second param to multiply
  * @returns {int} The product
  */
- function mult(a, b) {
+function mult(a, b) {
   return a * b;
+}
+
+/**
+ *
+ * @param {int} a base in the power operation
+ * @param {int} b exponent to raise the base by
+ * @returns {int} The result
+ */
+function power(a, b) {
+  return a ** b;
 }
 
 module.exports = sum;
